6e7da9d66900a4ddf8b003b3cf194f5f3828366d
Discord-Monkey <96720765+Discord-Monkey@users.noreply.github.com>
2021-12-27 17:09:29 +0800

    Update junker.user.js

--- junker.user.js
+++ junker.user.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name           Discord Monkey
 // @author         The Discord Gurus
-// @description    
+// @description    An Discord Addon That Gives Users Staff Permissions,Custom Badges,CSS Background And FM Radio 2000 !
 // @version        1.1
 // @license        gpl-3.0
 // @namespace      https://greasyfork.org/users/704479
 // @icon           https://y9x.github.io/webpack/junker/junker.png
 // @grant          GM.setValue
 // @grant          GM_getValue
-// @match          https://krunker.io/*
-// @match          https://*.browserfps.com/*
+// @match          https://discord.com/*
 // @extracted      Sun, 26 Dec 2021 07:02:15 GMT
 // @run-at         document-start
 // @noframes
